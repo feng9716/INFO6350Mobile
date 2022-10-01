d663e20354397a55f5cef7c945a1a411b17affd5
Zhening Feng <feng.zhen@northeastern.edu>
2022-10-01 16:55:24 -0700

    Get Request to get stock info

--- ClassExample/StockTicker/StockTicker/ViewController.swift
+++ ClassExample/StockTicker/StockTicker/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SwiftSpinner
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtStockSymbol: UITextField!
+    @IBOutlet weak var btnGetPrice: UIButton!
+    @IBOutlet weak var lblStock: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getStockPrice(_ sender:Any){
+        let apiKey = "b772c775d43a4cadd0dbafebef63f8fd"
+        var url = "https://financialmodelingprep.com/api/v3/quote/"
+        url += txtStockSymbol.text!
+        url += "?apikey="
+        url += apiKey
+        
+        SwiftSpinner.show("Fetching stock values")
+        
+        // url https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=b772c775d43a4cadd0dbafebef63f8fd
+        AF.request(url).responseJSON { response in
+//            print(response)
+            
+            SwiftSpinner.hide()
+            if(response.error != nil){
+                print(response.error!)
+                return
+            }
+            
+            guard let stock = JSON(response.data!).array?.first else {return}
+            
+            print(stock["name"].stringValue)
+            print(stock["price"].stringValue)
+        }
+        print(url)
+    }
 }
 
