41cfbde1a66b5d516acf9a8c25113d64c53b4a23
Zhening Feng <feng.zhen@northeastern.edu>
2022-11-05 14:46:20 -0700

    11/05

---
+++ ClassExample/PatientHealthChart/PatientHealthChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatientHealthChart
+//
+//  Created by 冯哲宁 on 11/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
